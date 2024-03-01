9468392833e8946e583a06e328511fb51bfa2b85
manishparihar948 <manish.parihar948@gmail.com>
2024-03-01 12:35:43 +0100

    Initial project setup

---
+++ WorldOfPay/WorldOfPay/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  WorldOfPay
+//
+//  Created by Manish Parihar on 01.03.24.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
---
+++ WorldOfPay/WorldOfPay/WorldOfPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfPayApp.swift
+//  WorldOfPay
+//
+//  Created by Manish Parihar on 01.03.24.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
